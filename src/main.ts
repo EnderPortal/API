f778571f521201cbc3e27bfe2c844777558a11d4
NKRI <nkri.dev@gmail.com>
2025-09-05 21:45:44 +0200

    fix: CORS error

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,17 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //Fixed: Cors error
+  app.enableCors({
+
+    /**
+     * Allow front
+     */
+    origin: "http://localhost:5173",
+    credentials: true
+  })
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
